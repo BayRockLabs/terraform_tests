dd1e13c73cf586475ff847da217836bf3116b366
Omkark111 <omkar.k@bayrocklabs.com>
2025-01-28 18:45:24 +0530

    encoded

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "c2c-demo"
     storage_account_name  = "tfc2c"
-    container_name        = "testbackend"
+    container_name        = "newtf"
     key                   = "tfstate-file"
   }
 }
